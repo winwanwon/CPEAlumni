9a61ccdfe03beff3ff3bc1aa967066a8d1dbc5cb
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-03-23 22:37:33 +0700

    test directory.php

--- application/views/directory.php
+++ application/views/directory.php
@@ -1,3 +1,10 @@
 <div class="container">
-
+  <form class="form-inline" id="undergrad_form"  style="display:none; margin: 8px auto;" >
+    <div class="form-group">
+      <label>AIAIAIAIAI</label>
+      <input type="number" id="gen" class="form-control input-sm" style="width: 100px;"placeholder="eg. 30">
+      <label>YAYAYAYAYA</label>
+      <input type="number" id="year" class="form-control input-sm" style="width: 100px;"placeholder="BE eg. 2559">
+    </div>
+  </form>
 </div>
