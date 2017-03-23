49b955d08a4af2c2ced4bfec87a0c514ec6018e7
Karis Matchaparn <oob130@hotmail.com>
2017-03-23 23:26:21 +0700

    test

--- application/views/directory.php
+++ application/views/directory.php
@@ -1,6 +1,5 @@
 <div class="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   <form class="form-inline" id="undergrad_form"  style="display:none; margin: 8px auto;" >
     <div class="form-group">
       <label>AIAIAIAIAI</label>
@@ -9,10 +8,9 @@
       <input type="number" id="year" class="form-control input-sm" style="width: 100px;"placeholder="BE eg. 2559">
     </div>
   </form>
-=======
+
     <p> aiaiaiaiaiya </p>
->>>>>>> origin/master
-=======
+
   <b>Winwanwon</b>
->>>>>>> origin/master
+
 </div>
