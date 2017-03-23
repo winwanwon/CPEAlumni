c8bbe67eeeca1d45f8be39ae5869d2033850f29f
Warat Kaweepornpoj <winwanwon123@gmail.com>
2017-03-23 22:45:41 +0700

    fix conflict

--- application/views/directory.php
+++ application/views/directory.php
@@ -1,6 +1,4 @@
 <div class="container">
-<<<<<<< HEAD
-<<<<<<< HEAD
   <form class="form-inline" id="undergrad_form"  style="display:none; margin: 8px auto;" >
     <div class="form-group">
       <label>AIAIAIAIAI</label>
@@ -9,10 +7,8 @@
       <input type="number" id="year" class="form-control input-sm" style="width: 100px;"placeholder="BE eg. 2559">
     </div>
   </form>
-=======
-    <p> aiaiaiaiaiya </p>
->>>>>>> origin/master
-=======
+  <p> aiaiaiaiaiya </p>
+  <p> aiaiaiaiaiya </p>
   <b>Winwanwon</b>
->>>>>>> origin/master
+
 </div>
