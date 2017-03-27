addff2346edec833426c7fd0326330477b5d920c
Yanisa Hemprachitchai <aiya.yanisa@gmail.com>
2017-03-28 01:30:06 +0700

    add fn getUser() and updateUser() in MODEL

--- application/models/Profile_model.php
+++ application/models/Profile_model.php
@@ -1,12 +1,12 @@
 <?php
 class Profile_model extends CI_Model {
 
-  public function __construct()
-  {
-    $this->load->database();
-  }
+public function __construct()
+{
+   $this->load->database();
+}
 
-  public function createUser(){
+public function createUser(){
 
     $data = array(
       'nameTitle' => $this->input->post('username'),
@@ -14,10 +14,29 @@ class Profile_model extends CI_Model {
       'lName' => $this->input->post('lastname'),
       'username' => $this->input->post('username'),
       'password' => sha1($this->input->post('password')),
-      'email' => $this->input->post('email'),
+      'email' => $this->input->post('email')
     );
-
 
     return $this->db->insert('student', $data);
   }
+
+public function getUser(){
+    $query = $this->db->get_where('student', array('username' => post('username')));
+    return $query->result();
+}
+
+public function updateUser(){
+    $data = array(
+      'nickname' => $this->input->post('nickname'),
+      'address' => $this->input->post('address'),
+      'province' => $this->input->post('province'),
+      'country' => $this->input->post('country'),
+      'facebook' => sha1($this->input->post('facebook')),
+      'linkedin' => $this->input->post('linkedin')
+    );
+
+    $this->db->where('username', post('username'));
+    return $this->db->update('student', $data);
+}
+
 }
